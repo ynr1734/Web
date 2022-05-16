323c20ccf4e5f273d638016044edb63ec20024de
ynr1734 <ynr1734@naver.com>
2022-05-16 23:55:02 +0900

    day044

---
+++ day044/src/main/java/com/shop/controller/MainController.java
@@ -0,0 +1,21 @@
+package com.shop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class MainController {
+	@RequestMapping("/")
+	public String main(Model m) {
+		m.addAttribute("center","center");
+		return "main";
+	}
+	
+	@RequestMapping("/html5")
+	public String html5(Model m) {
+		m.addAttribute("center","html5");
+		return "main";
+	}
+}
