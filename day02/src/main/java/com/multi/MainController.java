8e59baf356cc98b3ef2426c2390e9cf8784dbcfd
ynr1734 <ynr1734@naver.com>
2022-05-17 17:41:17 +0900

    day02

---
+++ day02/src/main/java/com/multi/MainController.java
@@ -0,0 +1,30 @@
+package com.multi;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+	@RequestMapping("/register")
+	public void register(String id, String pwd, int age) {
+		System.out.println(id+" "+pwd+" "+age);
+	}
+	@RequestMapping("/register2")
+	public void register2(String id, String pwd, int age, 
+			String birth, String gender, 
+			String hobby,String car, 
+			String resume, 
+			String loginid, int range  ) {
+		
+		System.out.println(id+" "+pwd+" "+age);
+		System.out.println(birth+" "+gender+" "+hobby);
+		System.out.println(resume+" "+loginid+" "+range);
+	}
+	
+}
+
+
+
+
+
