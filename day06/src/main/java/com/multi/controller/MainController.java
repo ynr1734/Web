f85cb1067034e984e7596b715e2ccfdd7d3d1369
ynr1734 <ynr1734@naver.com>
2022-05-21 23:05:10 +0900

    day06

---
+++ day06/src/main/java/com/multi/controller/MainController.java
@@ -0,0 +1,13 @@
+package com.multi.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+	@RequestMapping("/")
+	public String main(Model m) {
+		return "main";
+	}
+}
