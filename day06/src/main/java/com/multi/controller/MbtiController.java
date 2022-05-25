e1025902eccde8c35d6e28e3df81f30b1c59d2dd
ynr1734 <ynr1734@naver.com>
2022-05-25 17:24:10 +0900

    day06

--- day06/src/main/java/com/multi/controller/MbtiController.java
+++ day06/src/main/java/com/multi/controller/MbtiController.java
@@ -7,7 +7,118 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class MbtiController { 
 
+	@RequestMapping("/enfj")
+	public String enfj(Model m) {
+		m.addAttribute("center","mbti/enfj");
+		m.addAttribute("left","left");
+		return "main";
+	}
+
+	@RequestMapping("/enfp")
+	public String enfp(Model m) {
+		m.addAttribute("center","mbti/enfp");
+		m.addAttribute("left","left");
+		return "main";
+	}
+
+	@RequestMapping("/entj")
+	public String entj(Model m) {
+		m.addAttribute("center","mbti/entj");
+		m.addAttribute("left","left");
+		return "main";
+	}
 	
+	@RequestMapping("/entp")
+	public String entp(Model m) {
+		m.addAttribute("center","mbti/entp");
+		m.addAttribute("left","left");
+		return "main";
+	}
+	
+	@RequestMapping("/esfj")
+	public String esfj(Model m) {
+		m.addAttribute("center","mbti/esfj");
+		m.addAttribute("left","left");
+		return "main";
+	}
+	
+	@RequestMapping("/esfp")
+	public String esfp(Model m) {
+		m.addAttribute("center","mbti/esfp");
+		m.addAttribute("left","left");
+		return "main";
+	}
+	
+	@RequestMapping("/estj")
+	public String estj(Model m) {
+		m.addAttribute("center","mbti/estj");
+		m.addAttribute("left","left");
+		return "main";
+	}
+	
+	@RequestMapping("/estp")
+	public String estp(Model m) {
+		m.addAttribute("center","mbti/estp");
+		m.addAttribute("left","left");
+		return "main";
+	}
+
+	@RequestMapping("/infj")
+	public String infj(Model m) {
+		m.addAttribute("center","mbti/infj");
+		m.addAttribute("left","left");
+		return "main";
+	}
+
+	@RequestMapping("/infp")
+	public String infp(Model m) {
+		m.addAttribute("center","mbti/infp");
+		m.addAttribute("left","left");
+		return "main";
+	}
+
+	@RequestMapping("/intj")
+	public String intj(Model m) {
+		m.addAttribute("center","mbti/intj");
+		m.addAttribute("left","left");
+		return "main";
+	}
+	
+	@RequestMapping("/intp")
+	public String intp(Model m) {
+		m.addAttribute("center","mbti/intp");
+		m.addAttribute("left","left");
+		return "main";
+	}
+	
+	@RequestMapping("/isfj")
+	public String isfj(Model m) {
+		m.addAttribute("center","mbti/isfj");
+		m.addAttribute("left","left");
+		return "main";
+	}
+
+	@RequestMapping("/isfp")
+	public String isfp(Model m) {
+		m.addAttribute("center","mbti/isfp");
+		m.addAttribute("left","left");
+		return "main";
+	}
+
+	@RequestMapping("/istj")
+	public String istj(Model m) {
+		m.addAttribute("center","mbti/istj");
+		m.addAttribute("left","left");
+		return "main";
+	}
+
+	@RequestMapping("/istp")
+	public String istp(Model m) {
+		m.addAttribute("center","mbti/istp");
+		m.addAttribute("left","left");
+		return "main";
+	}
+
 	@RequestMapping("/mbti")
 	public String mbti(Model m) {
 		m.addAttribute("center","mbti/center");
@@ -41,6 +152,12 @@ public class MbtiController {
 	@RequestMapping("/contact")
 	public String contact(Model m) {
 		m.addAttribute("center","mbti/contact");
+		m.addAttribute("left","left");
+		return "main";
+	}
+	@RequestMapping("/location")
+	public String location(Model m) {
+		m.addAttribute("center","mbti/location");
 		m.addAttribute("left","left");
 		return "main";
 	}
---
+++ day06/src/main/resources/static/js/result.js
@@ -0,0 +1,95 @@
+$(document).ready(function(){
+	$('#enfj').click(function(){
+		location.href='https://www.16personalities.com/ko/%EC%84%B1%EA%B2%A9%EC%9C%A0%ED%98%95-enfj';
+	});
+});
+
+$(document).ready(function(){
+	$('#enfp').click(function(){
+		location.href='https://www.16personalities.com/ko/%EC%84%B1%EA%B2%A9%EC%9C%A0%ED%98%95-enfp';
+	});
+});
+
+$(document).ready(function(){
+	$('#entj').click(function(){
+		location.href='https://www.16personalities.com/ko/%EC%84%B1%EA%B2%A9%EC%9C%A0%ED%98%95-entj';
+	});
+});
+
+$(document).ready(function(){
+	$('#entp').click(function(){
+		location.href='https://www.16personalities.com/ko/%EC%84%B1%EA%B2%A9%EC%9C%A0%ED%98%95-entp';
+	});
+});
+
+$(document).ready(function(){
+	$('#esfj').click(function(){
+		location.href='https://www.16personalities.com/ko/%EC%84%B1%EA%B2%A9%EC%9C%A0%ED%98%95-esfj';
+	});
+});
+
+$(document).ready(function(){
+	$('#esfp').click(function(){
+		location.href='https://www.16personalities.com/ko/%EC%84%B1%EA%B2%A9%EC%9C%A0%ED%98%95-esfp';
+	});
+});
+
+$(document).ready(function(){
+	$('#estj').click(function(){
+		location.href='https://www.16personalities.com/ko/%EC%84%B1%EA%B2%A9%EC%9C%A0%ED%98%95-estj';
+	});
+});
+
+$(document).ready(function(){
+	$('#estp').click(function(){
+		location.href='https://www.16personalities.com/ko/%EC%84%B1%EA%B2%A9%EC%9C%A0%ED%98%95-estp';
+	});
+});
+
+$(document).ready(function(){
+	$('#infj').click(function(){
+		location.href='https://www.16personalities.com/ko/%EC%84%B1%EA%B2%A9%EC%9C%A0%ED%98%95-infj';
+	});
+});
+
+$(document).ready(function(){
+	$('#infp').click(function(){
+		location.href='https://www.16personalities.com/ko/%EC%84%B1%EA%B2%A9%EC%9C%A0%ED%98%95-infp';
+	});
+});
+
+$(document).ready(function(){
+	$('#intj').click(function(){
+		location.href='https://www.16personalities.com/ko/%EC%84%B1%EA%B2%A9%EC%9C%A0%ED%98%95-intj';
+	});
+});
+
+$(document).ready(function(){
+	$('#intp').click(function(){
+		location.href='https://www.16personalities.com/ko/%EC%84%B1%EA%B2%A9%EC%9C%A0%ED%98%95-intp';
+	});
+});
+
+$(document).ready(function(){
+	$('#isfj').click(function(){
+		location.href='https://www.16personalities.com/ko/%EC%84%B1%EA%B2%A9%EC%9C%A0%ED%98%95-isfj';
+	});
+});
+
+$(document).ready(function(){
+	$('#isfp').click(function(){
+		location.href='https://www.16personalities.com/ko/%EC%84%B1%EA%B2%A9%EC%9C%A0%ED%98%95-isfp';
+	});
+});
+
+$(document).ready(function(){
+	$('#istj').click(function(){
+		location.href='https://www.16personalities.com/ko/%EC%84%B1%EA%B2%A9%EC%9C%A0%ED%98%95-istj';
+	});
+});
+
+$(document).ready(function(){
+	$('#istp').click(function(){
+		location.href='istp';
+	});
+});
